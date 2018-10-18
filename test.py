af6f38a181eedf6b04ca90fe89ff23c106a30e2c
Keith Yu <keith.yu.ck@gmail.com>
2018-10-18 18:07:34 -0400

    prototype testing script created and ran successfully using pretrained VGG16, includes image preproccessing

---
+++ test.py
@@ -0,0 +1,24 @@
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+
+model = VGG16()
+
+# load an image from file
+image = load_img('plastic.jpg', target_size=(224, 224))
+
+# convert the imgae pixels to a numpy array
+image = img_to_array(image)
+
+# reshape data for the model
+image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+
+# predict the probability across all output classes
+yhat = model.predict(image)
+
+# convert the probabilities to class labels
+label = decode_predictions(yhat)
+# retrieve the most likely result, e.g. highest probability
+label = label[0][0]
+# print the classification
+print('%s (%.2f%%)' % (label[1], label[2]*100))
+
